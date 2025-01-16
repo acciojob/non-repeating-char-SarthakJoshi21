986b5f55a019302393093017a0d3469f004d278a
SarthakJoshi21 <157214236+SarthakJoshi21@users.noreply.github.com>
2025-01-16 21:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-	 const charCount = {};
+  const charCount = {};
 
+  // Count the frequency of each character in the string
   for (let char of str) {
     charCount[char] = (charCount[char] || 0) + 1;
   }
 
+  // Find and return the first character with a frequency of 1
   for (let char of str) {
     if (charCount[char] === 1) {
       return char;
     }
   }
 
-  return   
- null;
-	
+  // Return null if no non-repeated character is found
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
